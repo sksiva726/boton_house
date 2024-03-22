a5526081848f0ccb4eb6226932e252de3c057b6d
SKsiva726 <SKsivakumar726@gmail.com>
2024-03-22 21:31:24 +0530

    This is the commit for second time for webapplication

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ scalar = pickle.load(open('scaling.pkl', 'rb'))
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api')
+@app.route('/predict_api', methods = ['POST'])
 
 def predict_api():
     data = request.json['data']
@@ -21,6 +21,17 @@ def predict_api():
     output = regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+   data =[float(x) for x in request.form.values()] 
+   final_input = scalar.transform(np.array(data).reshape(1,-1))
+   print(final_input)
+   regmodel.predict(final_input)[0]
+   return render_template('home.html', prediction_text = "The House price prediction is {}".format(output))   
+
 
 if __name__ ==  "__main__":
     app.run(debug = True)
