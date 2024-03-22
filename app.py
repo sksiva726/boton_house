ec4707899cd74376d0dc923a23c0ed4d7867faf0
SKsiva726 <SKsivakumar726@gmail.com>
2024-03-22 22:16:06 +0530

    The web App is ready

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def predict():
    data =[float(x) for x in request.form.values()] 
    final_input = scalar.transform(np.array(data).reshape(1,-1))
    print(final_input)
-   regmodel.predict(final_input)[0]
+   output = regmodel.predict(final_input)[0]
    return render_template('home.html', prediction_text = "The House price prediction is {}".format(output))   
 
 
